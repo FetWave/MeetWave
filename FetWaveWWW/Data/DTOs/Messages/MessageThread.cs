42004d443de8c18c2c631d83ab2f8e36d7dec316
Drake Perrior-Small <drakecsmall@gmail.com>
2024-08-17 12:05:48 -0700

    Add message thread subject

--- FetWaveWWW/Data/DTOs/Messages/MessageThread.cs
+++ FetWaveWWW/Data/DTOs/Messages/MessageThread.cs
@@ -10,6 +10,7 @@ namespace FetWaveWWW.Data.DTOs.Messages
         public DateTime CreatedTS { get; set; } = DateTime.UtcNow;
         [Required]
         public string CreatedUserId { get; set; }
+        public string? Subject { get; set; }
 
         public virtual ICollection<MessageRecipient> Recipients { get; set; }
         public virtual ICollection<MessageLine> Lines { get; set; }
