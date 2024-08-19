d87018fd560393af940c02d82eb22107e0eebbf4
Drake Perrior-Small <drakecsmall@gmail.com>
2024-08-19 09:16:25 -0700

    Link message creator entity

--- FetWaveWWW/Data/DTOs/Messages/MessageThread.cs
+++ FetWaveWWW/Data/DTOs/Messages/MessageThread.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FetWaveWWW.Data.DTOs.Messages
 {
@@ -14,5 +16,7 @@ namespace FetWaveWWW.Data.DTOs.Messages
 
         public virtual ICollection<MessageRecipient> Recipients { get; set; }
         public virtual ICollection<MessageLine> Lines { get; set; }
+        [ForeignKey(nameof(CreatedUserId))]
+        public virtual IdentityUser CreatedUser { get; set; }
     }
 }
