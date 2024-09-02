c19542625d7c0dccaf8612769de08afc7fa4b5a1
Drake Perrior-Small <drakecsmall@gmail.com>
2024-09-01 19:18:32 -0700

    Add STRIPE integration

---
+++ FetWaveWWW/Services/Interfaces/IPaymentsService.cs
@@ -0,0 +1,7 @@
+﻿namespace MeetWave.Services.Interfaces
+{
+    public interface IPaymentsService
+    {
+        Task<string?> ChargeEventCover(long priceCents, long quantity, string? connectedAccount, long feePercent, string returnUrl);
+    }
+}
---
+++ FetWaveWWW/Services/StripePaymentsService.cs
@@ -0,0 +1,61 @@
+﻿using MeetWave.Services.Interfaces;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Stripe;
+using Stripe.Checkout;
+
+namespace MeetWave.Services
+{
+    public class StripePaymentsService : IPaymentsService
+    {
+
+        public StripePaymentsService(SessionService sessionService)
+        {
+        }
+
+        public async Task<string?> ChargeEventCover(long priceCents, long quantity, string? connectedAccount, long feePercent, string returnUrl)
+        {
+            try
+            {
+                var options = new SessionCreateOptions
+                {
+                    LineItems = new()
+                    {
+                        new()
+                        {
+                            PriceData = new()
+                            {
+                                Currency = "usd",
+                                ProductData = new()
+                                {
+                                    Name = "Event Cover",
+                                },
+                                UnitAmount = priceCents,
+                            },
+                            Quantity = quantity,
+                        },
+                    },
+                    PaymentIntentData = new()
+                    {
+                        ApplicationFeeAmount = (priceCents * feePercent) / 100
+                    },
+                    Mode = "payment",
+                    SuccessUrl = $"{returnUrl}?session_id={{CHECKOUT_SESSION_ID}}",
+                };
+                var requestOptions = new RequestOptions
+                {
+                    StripeAccount = connectedAccount
+                ,
+                };
+                var service = new SessionService();
+                var session = service.Create(options, requestOptions);
+
+                return session.Url;
+            }
+            catch (Exception ex)
+            {
+                var a = ex;
+            }
+            return null;
+        }
+    }
+}
