e6afdc0ef76fd1bd8148ce0344285b562ab86337
Drake Perrior-Small <drakecsmall@gmail.com>
2024-09-04 08:41:43 -0700

    Add SMTP Service

---
+++ FetWaveWWW/Services/SMTPEmailSender.cs
@@ -0,0 +1,30 @@
+﻿using MeetWave.Services.Interfaces;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+
+namespace MeetWave.Services
+{
+    public class SMTPEmailSender: IEmailSender, IExternalEmailSender
+    {
+        private readonly SmtpClient client;
+        public SMTPEmailSender(IConfiguration configuration) 
+        {
+
+            client = new SmtpClient(configuration["Authentication:SMTP:Server"], int.Parse(configuration["Authentication:SMTP:Port"]!));
+
+            client.Credentials = new System.Net.NetworkCredential(configuration["Authentication:SMTP:Username"], configuration["Authentication:SMTP:Password"]);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+        }
+
+        public Task EmailListAsync(IEnumerable<string> emails, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
